25c3c5ca6c732529ee95f03ffeb439c7d40d4d2f
Code4Nx <navrajshrestha1144@gmail.com>
2025-04-02 15:05:28 +0530

    updated

---
+++ app/src/main/java/com/example/chatapp/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.chatapp
+
+ const val USERS_COLLECTION = "users"
+
+const val USERS_REF = "users"
+
+const val CHATS_COLLECTION = "chats"
+
+const val FRIEND_COLLECTION = "friendList"
+
+const val MESSAGE_COLLECTION = "messages"
+
+const val AGORA_ID = BuildConfig.AGORA_APP_ID
