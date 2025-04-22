ae936946e17f7805124bdc093d8f20ebf903bf36
Code4Nx <navrajshrestha1144@gmail.com>
2025-04-22 19:30:56 +0530

    moved to manifest : setShowWhenLocked(true) and setTurnScreenOn(true)

--- app/src/main/java/com/example/chatapp/call/activity/CallActivity.kt
+++ app/src/main/java/com/example/chatapp/call/activity/CallActivity.kt
@@ -3,7 +3,6 @@ package com.example.chatapp.call.activity
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
@@ -14,8 +13,8 @@ import androidx.compose.runtime.getValue
 import com.example.chatapp.CALL_INTENT
 import com.example.chatapp.CallMetadata
 import com.example.chatapp.call.screen.CallScreen
-import com.example.chatapp.ui.theme.ChatsAppTheme
 import com.example.chatapp.call.viewmodel.CallViewModel
+import com.example.chatapp.ui.theme.ChatsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -61,17 +60,6 @@ class CallActivity : ComponentActivity() {
     private fun handleCallIntent(intent: Intent) {
 
         if (intent.action == CALL_INTENT){
-
-            @Suppress("DEPRECATION")
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-                setShowWhenLocked(true)
-                setTurnScreenOn(true)
-            } else {
-                window.addFlags(
-                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
-                            WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-                )
-            }
 
             val metaData = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 intent.getParcelableExtra("call_metadata", CallMetadata::class.java)
