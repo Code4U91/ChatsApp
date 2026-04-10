b9a29ff370b6a0277164ff3d510a6576dd4e12af
Code4Nx <navrajshrestha1144@gmail.com>
2026-04-10 18:25:03 +0530

    refactor : removed unused imports

--- app/src/main/java/com/example/chatapp/friend_feature/presentation/FriendListScreen.kt
+++ app/src/main/java/com/example/chatapp/friend_feature/presentation/FriendListScreen.kt
@@ -50,7 +50,6 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import com.example.chatapp.call_feature.presentation.call_history_screen.TopAppBarTemplate
 import com.example.chatapp.chat_feature.presentation.ChatItemAndFriendListItem
-import com.example.chatapp.friend_feature.presentation.FriendScreenUiItem
 import com.example.chatapp.shared.presentation.dialogBox.AddFriendDialogBox
 import com.example.chatapp.shared.presentation.viewmodel.GlobalMessageListenerViewModel
 
